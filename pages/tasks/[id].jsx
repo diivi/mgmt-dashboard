28679f7fa0994a025b12c6d2437f78ae668a6d12
Divyansh Singh <divyanshanandsingh@gmail.com>
2021-12-05 22:30:09 +0530

    sample page for each card

--- pages/tasks/[id].jsx
+++ pages/tasks/[id].jsx
@@ -1,10 +1,21 @@
 import { useRouter } from "next/router";
+import { initData } from "../../data/data";
+import moment from "moment";
 
 export default function TasksPage(props) {
+  const tasks = initData.tasks;
   const router = useRouter();
+  const taskId = parseInt(`${router.asPath.split("/")[2]}`);
+  const task = tasks.find((task) => task.id === taskId);
+
   return (
     <div>
-      <h1>Task {`${router.asPath}`}</h1>
+      <p style={{cursor:"pointer",color:"blue"}} onClick = {()=>router.push('/')}>Back</p>
+      <h1>Task {task.id} - {task.title}</h1>
+      <h4>{task.description}</h4>
+      <p>{moment(task.startDate).format("Do MMM YYYY")} - {moment(task.endDate).format("Do MMM YYYY")}</p>
+      <p>{task.percentageComplete}% Complete</p>
+      <ins>{task.owner}</ins>
     </div>
   );
 }
